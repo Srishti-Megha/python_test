82cd102663177d2ea19a8f886273078b54252f61
Srishti-Megha <41273795+Srishti-Megha@users.noreply.github.com>
2019-03-07 15:17:53 +0530

    Update prime_num.py

--- prime_num.py
+++ prime_num.py
@@ -1,11 +1,12 @@
-############## Check if number is a prime number and print Prime Number in the range (1, 100) #########################
+############## Check if given number is a prime number.
+############## Print Prime Number in the range (1, 100) #########################
 
 
 # number to check for prime number: num
 num = 81
 
 
-#### short code ########
+#### check if num is a prime number ########
 if all(num % j != 0 for j in range(2, num)):
     print(num, "is a prime number")
     
@@ -13,29 +14,10 @@ else:
     print(num, "is not a prime number")
 
 
-############# long Code ###########
-# Check if number is a prime number
-for i in range(2, num):
-#    print(i)
-    if num % i == 0:
-        print(num, "is not a prime number")
-        break
-       
-    elif i == num-1: 
-        print(num, "is a prime number")
-    
-    else:
-        continue
-    
-        
-
     
 # Print Prime Number in the range (1, 100)
-    
-l = [i for i in range(1, 100) if all(i % j != 0 for j in range(2, i) )]
+print([i for i in range(1, 100) if all(i % j != 0 for j in range(2, i) )])
 
-print(l)
-#print([i for i in range(1, 100) if all(i % j != 0 for j in range(2, i) )])
 
 
 
